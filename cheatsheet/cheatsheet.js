305173eea703cc8fa6ef569d1e2a16e1a7008217
Myranda Palomino <82559019+myranda920@users.noreply.github.com>
2021-08-09 21:37:56 -0700

    Add files via upload

---
+++ cheatsheet/cheatsheet.js
@@ -0,0 +1,40 @@
+// example of iterating through hashmap
+/** 
+ * 8 = mini
+ * 9 = junior
+ * 10 = junior
+ * 11 = junior
+ * 12 = junior 
+ * set variable to empty hashmap */
+const danceCompany = {};
+
+// Writing out the hashmap variable['key'] = 'value'
+// example with the ages
+
+danceCompany ['8'] = 'mini'
+danceCompany ['9'] = 'junior'
+danceCompany ['10'] = 'junior'
+danceCompany ['11'] = 'junior'
+danceCompany ['12'] = 'junior'
+
+// if variable is 'junior' push ages into jrAges Array
+
+const juniorAges = [];
+// setting variable empty array 
+for (k in danceCompany);
+    if ('junior' == danceCompany[k]){
+        // if value is equal to junior 
+        juniorAges.push(k)
+      //array.function(value)  
+      // push ages into jrAges array
+    }
+    return juniorAges;
+
+
+    //functions
+    //function(name{
+        //console.log('hi' + name)
+    //}
+    //function()
+    //function('Art')
+    // console.log hi Art 
